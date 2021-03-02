f1df0fb296eabe0bc61c704f7abbba64640f4c9e
小柒2012 <345849402@qq.com>
2021-03-02 20:34:47 +0800

    菜单目录删除逻辑判断

--- src/main/java/com/tools/module/sys/service/impl/SysMenuServiceImpl.java
+++ src/main/java/com/tools/module/sys/service/impl/SysMenuServiceImpl.java
@@ -50,13 +50,13 @@ public class SysMenuServiceImpl implements SysMenuService {
         String nativeSql = "SELECT * FROM sys_menu WHERE menu_id = ?";
         SysMenu menu =
                 dynamicQuery.nativeQuerySingleResult(SysMenu.class,nativeSql,new Object[]{menuId});
-        if(menu.getType().equals(SystemConstant.MenuType.CATALOG)){
+        if(menu.getType().equals(SystemConstant.MenuType.CATALOG.getValue())){
             nativeSql = "SELECT COUNT(*) FROM sys_menu WHERE parent_id=?";
             Long count = dynamicQuery.nativeQueryCount(nativeSql,new Object[]{menuId});
             if(count>0){
                 return Result.error("请先删除下级目录、菜单或者按钮");
             }
-        }else if(menu.getType().equals(SystemConstant.MenuType.MENU)){
+        }else if(menu.getType().equals(SystemConstant.MenuType.MENU.getValue())){
             /**
              * 删除下级按钮
              */
